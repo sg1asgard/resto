c8000224fd8c4abb463297d13f4a1843213d9d66
George A. <sg1asgard@outlook.com>
2024-06-24 16:43:35 +0300

    wip

--- app/routes/menu.tsx
+++ app/routes/menu.tsx
@@ -1,9 +1,5 @@
 import type { MetaFunction } from '@remix-run/node'
-import { createClient } from '@supabase/supabase-js'
 import Menu from 'public/components/MenuComponent'
-
-// Create a single supabase client for interacting with your database
-// const supabase = createClient(`${process.env.SUPA_PROJECT_URL}`, `${process.env.SUPA_API_KEY}`)
 
 export const meta: MetaFunction = () => {
   return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
@@ -13,9 +9,7 @@ export default function Index() {
   return (
     <>
       <Menu />
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold">Restaurant Menu</h1>
     </>
   )
 }
---
+++ public/components/MenuComponent.tsx
@@ -0,0 +1,12 @@
+import { Link } from '@remix-run/react'
+
+export default function Menu() {
+    return (
+        <>
+            <div className='flex py-5 my-5'>
+                <Link to="/" className="capitalize">home</Link>
+                <Link to="/menu" className="capitalize">menu</Link>
+            </div>
+        </>
+    )
+}
