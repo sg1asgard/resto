f9a3f2350514c3b3aee1ac52bbc539ad619babd4
George A. <sg1asgard@outlook.com>
2024-06-24 16:05:23 +0300

    wip

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,12 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node'
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(`${process.env.SUPA_PROJECT_URL}`, `${process.env.SUPA_API_KEY}`)
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+  return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
+}
 
 export default function Index() {
   return (
@@ -44,5 +45,5 @@ export default function Index() {
         </li>
       </ul>
     </div>
-  );
+  )
 }
