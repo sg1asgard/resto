5f5474f5a36932cf241fb343c9bc0fb6726fe57c
George A. <sg1asgard@outlook.com>
2024-06-24 17:30:40 +0300

    wip

--- app/routes/menu.tsx
+++ app/routes/menu.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from '@remix-run/node'
+import menuStyle from '~/components/MenuStyle.css'
 
 export const meta: MetaFunction = () => {
   return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }]
@@ -6,8 +7,12 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <>
+    <div className="repo-menu">
       <h1 className="text-3xl font-bold">Restaurant Menu</h1>
-    </>
+    </div>
   )
+}
+
+export function links() {
+  [{ rel: 'stylesheet', href: menuStyle}]
 }
